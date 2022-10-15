22440a39e3ff62c17130fbd508503aa0b063a0b5
Mehul Saxena <mehulsaxena45@gmail.com>
2022-10-16 02:16:47 +0530

    Check

--- js/main.js
+++ js/main.js
@@ -1,13 +1,33 @@
-let Song = Backbone.Model.extend();
+let Song = Backbone.Model.extend({
+    defaults : {
+        listeners : 0
+    }
+});
 
 let Songs = Backbone.Collection.extend({
     model : Song
+
 });
 
 let SongView = Backbone.View.extend({
     tagName: 'li',
+    initialize : function(){
+        // this.model.on('change' , this.render , this);
+        this.model.on('change' , this.onModelChange, this)
+    },
+    events:{
+        "click" : "onClick",
+        "click .bookmark" : 'onClickBookmark' 
+    },
+    onClick: function (){
+        console.log('Listen Clicked')
+    },
+    onClickBookmark : function(e){
+        e.stopPropagation();
+        console.log('Book mark is clicked')
+    } ,
     render: function(){
-        this.$el.html(this.model.get('title'));
+        this.$el.html(this.model.get('title') + "<button>Listen</button> <button class='bookmark' > Bookmark </button>" + " - Listerners: " + this.model.get('listeners'));
         return this;
     }
 });
@@ -29,5 +49,43 @@ let songs = new Songs([
     new Song({title : 'All blues'})
 ]);
 
+let song = new Song();
+
 let songsView = new SongsView({el:'#songs' , model : songs});
 songsView.render();
+
+let DropBoxCollection = Backbone.Collection.extend({
+    model:Song
+});
+
+let DropView = Backbone.View.extend({
+    tagName : 'option',
+    initialize : function(){
+        console.log('this is working')
+    },
+    render : function(){
+        this.$el.html(this.model.get('title'));
+        return this;
+    }
+})
+
+let DropsView = Backbone.View.extend({
+    render:function(){
+        let self =this;
+        this.model.each(function(song){
+            let dropView = new DropView({model : song});
+            self.$el.append(dropView.render().$el);
+        })
+    }
+})
+
+let dropSongs = new DropBoxCollection([
+    new Song({title : 'Blue in green'}),
+    new Song({title : 'so what'}),
+    new Song({title : 'All blues'})
+])
+
+
+
+let dropBoxView = new DropsView({el:'#dropBox' , model:dropSongs});
+dropBoxView.render()
